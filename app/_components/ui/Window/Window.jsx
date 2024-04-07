74237f4f4ac588f56bd023643995916e6061e5c4
BURmister <olegcernysov903@gmail.com>
2024-04-08 00:42:30 +0300

    pretty sort & filter window

--- app/_components/ui/Window/Window.jsx
+++ app/_components/ui/Window/Window.jsx
@@ -1,42 +1,42 @@
 'use client';
-import { useState, useEffect } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { SquareIconButton } from '../Button/Button';
 
 import styles from './styles.module.scss';
 
-export const Window = () => {
-   const [open, setOpen] = useState(true);
+export const Window = ({ children, title, buttonIcon, rightStyle = false }) => {
+   const [open, setOpen] = useState(false);
+   const windowRef = useRef(null);
 
-   const handleToggle = () => {
-      setOpen((prev) => !prev);
-   };
+   const handleToggle = () => setOpen((prev) => !prev);
    const handleClose = () => setOpen(false);
+   const handleClickOutside = (event) => {
+      if (windowRef.current && !windowRef.current.contains(event.target)) handleClose();
+   };
 
-   useEffect(() => console.log('window: ', open), [open]);
+   useEffect(() => {
+      document.addEventListener('click', handleClickOutside);
+      return () => {
+         // Unbind the event listener on clean up
+         document.removeEventListener('click', handleClickOutside);
+      };
+   }, [windowRef]);
 
    return (
-      <div className={`relative`}>
-         <SquareIconButton
-            icon={
-               <svg stroke="var(--black)" fill="var(--black)" strokeWidth="0" viewBox="0 0 256 256" height="48px" width="48px">
-                  <path d="M209.15,60.17l-80-24A4,4,0,0,0,124,40V158.75A44,44,0,1,0,132,184V93.38l74.85,22.45A4,4,0,0,0,212,112V64A4,4,0,0,0,209.15,60.17ZM88,220a36,36,0,1,1,36-36A36,36,0,0,1,88,220ZM204,106.62,132,85V45.38L204,67Z"></path>
-               </svg>
-            }
-            title={'Сортировка'}
-            onClick={() => handleToggle()}
-         />
-         <div className={`${styles.window} ${open ? styles.open : ''}`}>
-            <div className={styles.overlay}></div>
-            <div className={styles.wrapper}>
-               <div className={styles.header}>
-                  <span></span>
-                  <h2 className="caption-24">Сортировка</h2>
-                  <button type="button" className={`caption-24 ${styles.closeButton}`} onClick={() => handleClose()}>
-                     X
-                  </button>
-               </div>
-               <div className={styles.content}>
-                  <div>сортировка | контент</div>
+      <div ref={windowRef} className={`relative`}>
+         <SquareIconButton icon={buttonIcon} title={title} onClick={() => handleToggle()} />
+         <div className={`${styles.wrapper} ${open ? styles.open : ''} ${rightStyle && styles.rightStyle}`}>
+            <div className={styles.overlay} onClick={() => handleClose()}></div>
+            <div className={styles.windowWrapper}>
+               <div className={`flex flex-col ${styles.window}`}>
+                  <div className={styles.header}>
+                     <span className="invisible">X</span>
+                     <h2 className="caption-24">{title}</h2>
+                     <button type="button" className={`caption-24 ${styles.closeButton}`} onClick={() => handleClose()}>
+                        X
+                     </button>
+                  </div>
+                  <div className={styles.content}>{children}</div>
                </div>
             </div>
          </div>
